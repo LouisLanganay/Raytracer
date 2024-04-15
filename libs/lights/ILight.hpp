86818d3137a5711917d702d6a1d646a96e470b96
LouisLanganay <louislanganay@gmail.com>
2024-04-15 11:55:09 +0200

    feat: Add ILight and IMaterial interfaces
    Added the ILight and IMaterial interfaces to the project. These interfaces are used for handling lights and materials in the raytracer.

---
+++ libs/lights/ILight.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** ILight
+*/
+
+#ifndef ILIGHT_HPP_
+#define ILIGHT_HPP_
+
+namespace RayTracer {
+    class ILight {
+        public:
+            ILight();
+            ~ILight();
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !ILIGHT_HPP_ */
---
+++ libs/materials/IMaterial.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** IMaterial
+*/
+
+#ifndef IMATERIAL_HPP_
+#define IMATERIAL_HPP_
+
+namespace RayTracer {
+    class IMaterial {
+        public:
+            IMaterial();
+            ~IMaterial();
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !IMATERIAL_HPP_ */
