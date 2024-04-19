96d69912ae31d4ad00785ad2bcd6f56a52313481
LouisLanganay <louislanganay@gmail.com>
2024-04-19 22:03:18 +0200

    feat: ILights interface
    Signed-off-by: LouisLanganay <louislanganay@gmail.com>

--- libs/lights/ILight.hpp
+++ libs/lights/ILight.hpp
@@ -8,12 +8,23 @@
 #ifndef ILIGHT_HPP_
 #define ILIGHT_HPP_
 
-namespace RayTracer {
+#include "../../src/Point/Point3D.hpp"
+#include <string>
+
+namespace RayTracer::Lights {
     class ILight {
         public:
-            ILight();
-            ~ILight();
+            virtual ~ILight() = default;
 
+            virtual void setType(const std::string &type) = 0;
+            virtual void setOrigin(double x, double y, double z) = 0;
+            virtual void setColor(double x, double y, double z) = 0;
+            virtual void setIntensity(double intensity) = 0;
+
+            virtual std::string getType() const = 0;
+            virtual Point3D getOrigin() const = 0;
+            virtual Point3D getColor() const = 0;
+            virtual double getIntensity() const = 0;
         protected:
         private:
     };
