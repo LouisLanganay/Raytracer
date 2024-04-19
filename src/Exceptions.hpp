5baf199ab5c0f5981d444f9af43e55176926bc11
LouisLanganay <louislanganay@gmail.com>
2024-04-19 22:00:44 +0200

    feat: Exceptions for parser, lights, primitives
    Signed-off-by: LouisLanganay <louislanganay@gmail.com>

---
+++ src/Exceptions.hpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** Exceptions
+*/
+
+#ifndef EXCEPTIONS_HPP_
+#define EXCEPTIONS_HPP_
+
+#include <exception>
+#include <string>
+
+namespace RayTracer {
+    class Exception : public std::exception {
+        public:
+            Exception(const std::string &message) : _message(message) {}
+            const char *what() const noexcept override { return _message.c_str(); }
+
+        protected:
+        private:
+            std::string _message;
+    };
+
+    class ParserException : public Exception {
+        public:
+            ParserException(const std::string &message) : Exception(message) {}
+    };
+
+    class CameraException : public Exception {
+        public:
+            CameraException(const std::string &message) : Exception(message) {}
+    };
+
+    class LightException : public Exception {
+        public:
+            LightException(const std::string &message) : Exception(message) {}
+    };
+
+    class PrimitiveException : public Exception {
+        public:
+            PrimitiveException(const std::string &message) : Exception(message) {}
+    };
+}
+#endif /* !EXCEPTIONS_HPP_ */
