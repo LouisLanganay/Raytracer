97e61b7f4e455b85b47c4e7863feb02009c11b17
LouisLanganay <louislanganay@gmail.com>
2024-04-19 22:00:01 +0200

    feat: Add AmbienteLight files
    Signed-off-by: LouisLanganay <louislanganay@gmail.com>

---
+++ libs/lights/ambienteLight/AmbienteLight.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** ambienteLight
+*/
+
+#ifndef AMBIENTELIGHT_HPP_
+#define AMBIENTELIGHT_HPP_
+
+#include "../ALight.hpp"
+
+namespace RayTracer::Lights {
+    class AmbientLight : public ALight {
+        public:
+            AmbientLight();
+            ~AmbientLight();
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !AMBIENTELIGHT_HPP_ */
