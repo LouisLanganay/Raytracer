a82e5703d9424ccc5f536c9600d896ca05ffc5a5
LouisLanganay <louislanganay@gmail.com>
2024-04-28 14:50:36 +0200

    feat CameraBuilder and Primitive- AddBuilder class to handle the creation objectsBuilder class the of Render objects - Add LightBuilder class to handle the creation of Light objects- Add PrimitiveBuilder to the creation of Primitive objects

---
+++ src/Builders/LightBuilder.hpp
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** LightBuilder
+*/
+
+#ifndef LIGHTBUILDER_HPP_
+#define LIGHTBUILDER_HPP_
+
+#include "Point3D.hpp"
+#include <iostream>
+#include <memory>
+#include <string>
+#include "../../libs/lights/ALight.hpp"
+
+namespace RayTracer::Builders {
+    class LightBuilder {
+        private:
+            Lights::ILight *_light;
+
+        public:
+            LightBuilder(RayTracer::Lights::ILight *light)
+            {
+                _light = light;
+            }
+
+            LightBuilder& set(const std::string &key, Point3D value)
+            {
+                if (key == "position")
+                    _light->setOrigin(value._x, value._y, value._z);
+                return *this;
+            }
+
+            LightBuilder& set(const std::string &key, Vector3D value)
+            {
+                if (key == "color")
+                    _light->setColor(value._x, value._y, value._z);
+                return *this;
+            }
+
+            LightBuilder& set(const std::string &key, double value)
+            {
+                if (key == "intensity")
+                    _light->setIntensity(value);
+                return *this;
+            }
+
+            LightBuilder& set(const std::string &key, const std::string &value)
+            {
+                if (key == "type")
+                    _light->setType(value);
+                return *this;
+            }
+    };
+}
+
+#endif /* !LIGHTBUILDER_HPP_ */
---
+++ src/Builders/PrimitiveBuilder.hpp
@@ -0,0 +1,83 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** PrimitiveBuilder
+*/
+
+#ifndef PRIMITIVEBUILDER_HPP_
+#define PRIMITIVEBUILDER_HPP_
+
+#include "Point3D.hpp"
+#include <iostream>
+#include <memory>
+#include <string>
+#include "../../libs/primitives/APrimitive.hpp"
+#include "Exceptions.hpp"
+#include <libconfig.h++>
+
+
+namespace RayTracer::Builders {
+    class PrimitiveBuilder {
+        private:
+            Primitives::IPrimitive *_primitive;
+
+        public:
+            PrimitiveBuilder(RayTracer::Primitives::IPrimitive *primitive)
+            {
+                _primitive = primitive;
+            }
+
+            PrimitiveBuilder& set(const std::string &key, Point3D value)
+            {
+                if (key == "origin")
+                    _primitive->setOrigin(value._x, value._y, value._z);
+                if (key == "rotation")
+                    _primitive->setRotation(value._x, value._y, value._z);
+                return *this;
+            }
+
+            PrimitiveBuilder& set(const std::string &key, double value)
+            {
+                if (key == "radius")
+                    _primitive->setRadius(value);
+                if (key == "height")
+                    _primitive->setHeight(value);
+                if (key == "width")
+                    _primitive->setWidth(value);
+                return *this;
+            }
+
+            PrimitiveBuilder& set(const std::string &key, const std::string &value)
+            {
+                if (key == "type")
+                    _primitive->setType(value);
+                return *this;
+            }
+
+            PrimitiveBuilder& set(const std::string &key, const libconfig::Setting &setting)
+            {
+                if (key == "axis") {
+                    std::string arg = setting["axis"];
+                    if (arg == "Z")
+                        _primitive->setAxis(RayTracer::Primitives::Axis::Z);
+                    else if (arg == "Y")
+                        _primitive->setAxis(RayTracer::Primitives::Axis::Y);
+                    else if (arg == "X")
+                        _primitive->setAxis(RayTracer::Primitives::Axis::X);
+                    else
+                        throw ParserException("Unknown axis");
+                }
+                return *this;
+            }
+
+            PrimitiveBuilder& set(const std::string &key, const std::shared_ptr<RayTracer::Materials::IMaterial> &value)
+            {
+                if (key == "material")
+                    _primitive->setMaterial(value);
+                return *this;
+            }
+    };
+}
+
+#endif /* !PRIMITIVEBUILDER_HPP_ */
