47d1ea9767ab5269034916a4664d5c83c6d8aa2a
blaizot <guillaume.blaizot@epitech.eu>
2024-05-08 19:24:36 +0200

    refactor: Add Vector3D include statement to Point3D.cpp

--- src/Vector/Vector3D.hpp
+++ src/Vector/Vector3D.hpp
@@ -8,11 +8,14 @@
 #ifndef VECTOR3D_HPP_
 #define VECTOR3D_HPP_
 
+class Point3D;
+
 class Vector3D {
     public:
         Vector3D();
         ~Vector3D() = default;
         Vector3D(double x, double y, double z);
+        Vector3D(const Point3D& other);
 
         Vector3D(const Vector3D& other) = default;
 
@@ -25,18 +28,23 @@ class Vector3D {
         double dot(const Vector3D& other) const;
         double lengthSquared() const;
 
+        Vector3D operator==(const Vector3D& other) const;
         Vector3D operator+(const Vector3D& other) const;
+        Vector3D operator+(const Point3D& other) const;
         Vector3D& operator+=(const Vector3D& other);
         Vector3D operator-(const Vector3D& other) const;
+        Vector3D operator-(const Point3D& other) const;
         Vector3D& operator-=(const Vector3D& other);
         Vector3D operator*(double scalar) const;
         Vector3D& operator*=(double scalar);
+        Vector3D &operator*=(const Vector3D &other);
         Vector3D operator/(double scalar) const;
         Vector3D& operator/=(double scalar);
         Vector3D operator*(const Vector3D& other) const;
         Vector3D operator-() const;
 
         void normalize();
+        bool isZero() const;
         Vector3D getNormalized() const;
         Vector3D cross(const Vector3D& other) const;
         void clamp(double min, double max);
