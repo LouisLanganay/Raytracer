f059435a0961c4213a4c4b588931dcdb305cf10a
Valentin Peron <valentin.peron@epitech.eu>
2024-05-11 19:55:28 +0200

    Refactor Flat class to use HitRecord instead of RayHit

--- libs/materials/Flat/Flat.hpp
+++ libs/materials/Flat/Flat.hpp
@@ -10,17 +10,25 @@
 
 #include "../AMaterial.hpp"
 
+namespace RayTracer {
+    class Ray;
+    class HitRecord;
+    class Vector3D;
+}
+
 namespace RayTracer::Materials {
+
     class Flat : public AMaterial {
         public:
-            Flat();
-            ~Flat();
+            Flat() = default;
+            ~Flat() override = default;
 
-            bool scatter(const Ray &ray, const RayHit &hit, Scatter &scatter) const override;
-            Vector3D getColor(const Ray &ray, const RayHit &hit) const override;
-        protected:
+            bool scatter(const Ray &ray, HitRecord &rec, Vector3D &attenuation, Ray &scattered) const override;
+            Vector3D getColor(const Ray &ray, const HitRecord &hit) override;
         private:
+            // Add any private members here if needed
     };
+
 }
 
 #endif /* !FLAT_HPP_ */
