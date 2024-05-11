3f48843c90e66d22cb9371d7a53f322331d8562b
Valentin Peron <valentin.peron@epitech.eu>
2024-05-11 19:54:54 +0200

    Refactor computeLights function in AmbientLight.cpp

--- libs/lights/AmbientLight/AmbientLight.cpp
+++ libs/lights/AmbientLight/AmbientLight.cpp
@@ -17,7 +17,8 @@ namespace RayTracer::Lights {
     Vector3D AmbientLight::computeLights(
             Vector3D color,
             const Ray &ray,
-            const RayHit &hit,
+            // const RayHit &hit,
+            const HitRecord &hit,
             const std::vector<RayTracer::Primitives::IPrimitive *> &primitives
     ) const
     {
