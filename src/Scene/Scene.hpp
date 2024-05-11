5df27201aedde4cb822e6553f91a28050209be74
Valentin Peron <valentin.peron@epitech.eu>
2024-05-11 19:57:11 +0200

    Refactor Scene.hpp to use HitRecord instead of RayHit and Interval

--- src/Scene/Scene.hpp
+++ src/Scene/Scene.hpp
@@ -13,8 +13,20 @@
 #include "../../libs/primitives/IPrimitive.hpp"
 #include "../../libs/lights/ILight.hpp"
 #include "../Camera/Camera.hpp"
+#include "../Interval/Interval.hpp"
+#include "../../libs/materials/AMaterial.hpp"
 
 namespace RayTracer {
+    // class HitRecord {
+    //     public:
+    //         Point3D p;
+    //         Vector3D normal;
+    //         std::shared_ptr<Materials::AMaterial> mat_ptr;
+    //         double t;
+    //         bool front_face;
+
+    //         void set_face_normal(const Ray &r, const Vector3D &outward_normal); 
+    // };
     class Scene {
         public:
             Scene();
@@ -28,6 +40,9 @@ namespace RayTracer {
             const std::vector<Lights::ILight *> &getLights() const;
             Camera *getCamera() const;
 
+            Vector3D ray_Color(const Ray &ray, int depth, const Scene &scene);
+            Ray get_ray(double u, double v) const;
+            bool hit(const Ray &r, const Interval interval, HitRecord &rec) const;
         protected:
         private:
             std::vector<Primitives::IPrimitive *> _primitives;
