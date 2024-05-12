4cb46d31079b3306cd710563f1aa408f9c79755f
Valentin Peron <valentin.peron@epitech.eu>
2024-05-12 18:49:52 +0200

    Refactor render methods in PNGRender and IRender.hpp

--- src/Graphical.cpp
+++ src/Graphical.cpp
@@ -11,14 +11,14 @@
 Graphical::Graphical(char *path)
 {
     window.create(sf::VideoMode(1920, 1080), "Raytracer");
-    texture.create(1920, 1080);
-    sprite.setTexture(texture);
-    sprite.scale(2, 2);
+
     font.loadFromFile("assets/font.ttf");
     text.setFont(font);
     text.setCharacterSize(24);
     text.setFillColor(sf::Color::White);
+    RenderedImage.setScale(4, 4);
     this->path = path;
+    currentMode = mode::VIDEO;
 }
 
 Graphical::~Graphical()
@@ -26,107 +26,150 @@ Graphical::~Graphical()
     window.close();
 }
 
-void Graphical::eventLoop()
+void Graphical::renderVideo()
 {
+    window.clear(sf::Color::Black);
+    window.draw(RenderedImage);
+    time = clock.getElapsedTime();
+    text.setString("FPS: " + std::to_string(1.0f / time.asSeconds()));
+    clock.restart();
+    text.setPosition(0, 0);
+    window.draw(text);
+    text.setPosition(500, 0);
+    text.setString("X: " + std::to_string(camera->getOrigin()._x) + " Y: " + std::to_string(camera->getOrigin()._y) + " Z: " + std::to_string(camera->getOrigin()._z));
+    window.draw(text);
+    window.display();
+}
 
+void Graphical::renderMenu()
+{
+    window.clear(sf::Color::Black);
+    text.setString("Samples: " + std::to_string(render->getSamples()));
+    text.setPosition(0, 0);
+    window.draw(text);
+    text.setString("Max Depth: " + std::to_string(render->getMaxDepth()));
+    text.setPosition(0, 50);
+    window.draw(text);
+    window.display();
+}
+
+void Graphical::handleEvents()
+{
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+            window.close();
+        if (event.type == sf::Event::KeyReleased) {
+            if (event.key.code == sf::Keyboard::Escape) {
+                (currentMode == mode::VIDEO) ? currentMode = mode::MENU : currentMode = mode::VIDEO;
+            }
+        }
+    }
+    (currentMode == mode::VIDEO) ? handleVideoEvents() : handleMenuEvents();
+}
+
+void Graphical::handleMenuEvents()
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        if (render->getSamples() < 16)
+            render->setSamples(render->getSamples() + 1);
+        sf::sleep(sf::milliseconds(100));
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        if (render->getSamples() > 1)
+            render->setSamples(render->getSamples() - 1);
+        sf::sleep(sf::milliseconds(100));
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        if (render->getMaxDepth() < 16)
+            render->setMaxDepth(render->getMaxDepth() + 1);
+        sf::sleep(sf::milliseconds(100));
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        if (render->getMaxDepth() > 1)
+            render->setMaxDepth(render->getMaxDepth() - 1);
+        sf::sleep(sf::milliseconds(100));
+    }
+}
+
+void Graphical::handleVideoEvents()
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+        Point3D pos = camera->getOrigin();
+        camera->setOrigin(pos._x + 100, pos._y, pos._z);
+        image = render->renderVideo(*scene, *camera);
+        RenderedTexture.update(image);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        Point3D pos = camera->getOrigin();
+        camera->setOrigin(pos._x - 100, pos._y, pos._z);
+        image = render->renderVideo(*scene, *camera);
+        RenderedTexture.update(image);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        Point3D pos = camera->getOrigin();
+        camera->setOrigin(pos._x, pos._y + 100, pos._z);
+        image = render->renderVideo(*scene, *camera);
+        RenderedTexture.update(image);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+        Point3D pos = camera->getOrigin();
+        camera->setOrigin(pos._x, pos._y - 100, pos._z);
+        image = render->renderVideo(*scene, *camera);
+        RenderedTexture.update(image);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        Point3D pos = camera->getOrigin();
+        camera->setOrigin(pos._x, pos._y, pos._z + 100);
+        image = render->renderVideo(*scene, *camera);
+        RenderedTexture.update(image);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)) {
+        Point3D pos = camera->getOrigin();
+        camera->setRotation(0, 0, 0);
+        camera->setOrigin(pos._x, pos._y, pos._z - 100);
+        image = render->renderVideo(*scene, *camera);
+        RenderedTexture.update(image);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        double fov = camera->getFov();
+        camera->setFov(fov + 10);
+        image = render->renderVideo(*scene, *camera);
+        RenderedTexture.update(image);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        double fov = camera->getFov();
+        camera->setFov(fov - 10);
+        image = render->renderVideo(*scene, *camera);
+        RenderedTexture.update(image);
+    }
 }
 
 void Graphical::run()
 {
-    RayTracer::LibLoader libLoader;
     libLoader.loadPlugins();
     RayTracer::Parser parser(path, libLoader);
     parser.parse();
     scene = std::move(parser.getScene());
-    std::unique_ptr<RayTracer::Render::IRender> render = parser.getRender();
+    render = parser.getRender();
     render->setSamples(1);
     render->setMaxDepth(0);
-    
-    sf::Image image;
-    sf::Sprite RenderedImage;
-    sf::Texture RenderedTexture;
-    
-    //Camera
-    RayTracer::Camera *camera = scene->getCamera();
-    camera->setResolution(1920, 1080);
-    camera->setFov(90);
-    // camera->setOrigin(0, 0, 0);
-    camera->setRotation(0, 0, 0);
+        
+    camera = scene->getCamera();
+    camera->setResolution(1920 / 4, 1080 / 4);
 
     image = render->renderVideo(*scene, *camera);
-
-
 
     RenderedTexture.create(1920, 1080);
     RenderedImage.setTexture(RenderedTexture);
     RenderedTexture.update(image);
 
     Point3D pos = camera->getOrigin();
-
     
-    sf::Event event;
-
     while (window.isOpen()) {
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape)
-                window.close();
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-                Point3D pos = camera->getOrigin();
-                camera->setOrigin(pos._x + 100, pos._y, pos._z);
-                image = render->renderVideo(*scene, *camera);
-                RenderedTexture.update(image);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-                Point3D pos = camera->getOrigin();
-                camera->setOrigin(pos._x - 100, pos._y, pos._z);
-                image = render->renderVideo(*scene, *camera);
-                RenderedTexture.update(image);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-                Point3D pos = camera->getOrigin();
-                camera->setOrigin(pos._x, pos._y + 100, pos._z);
-                image = render->renderVideo(*scene, *camera);
-                RenderedTexture.update(image);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
-                Point3D pos = camera->getOrigin();
-                camera->setOrigin(pos._x, pos._y - 100, pos._z);
-                image = render->renderVideo(*scene, *camera);
-                RenderedTexture.update(image);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-                Point3D pos = camera->getOrigin();
-                camera->setOrigin(pos._x, pos._y, pos._z + 100);
-                image = render->renderVideo(*scene, *camera);
-                RenderedTexture.update(image);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)) {
-                Point3D pos = camera->getOrigin();
-                camera->setOrigin(pos._x, pos._y, pos._z - 100);
-                image = render->renderVideo(*scene, *camera);
-                RenderedTexture.update(image);
-            }
+        if (clock.getElapsedTime().asSeconds() > 0.3) {
+            clock.restart();
         }
-        window.clear(sf::Color::Black);
-        window.draw(RenderedImage);
-        time = clock.getElapsedTime();
-        text.setString("FPS: " + std::to_string(1.0f / time.asSeconds()));
-        clock.restart();
-        text.setPosition(0, 0);
-        window.draw(text);
-        text.setPosition(500, 0);
-        text.setString("X: " + std::to_string(camera->getOrigin()._x) + " Y: " + std::to_string(camera->getOrigin()._y) + " Z: " + std::to_string(camera->getOrigin()._z));
-        window.draw(text);
-
-        
-
-
-        // image = render->renderVideo(*scene, *camera);
-        // RenderedTexture.update(image);
-        // RenderedImage.setTexture(RenderedTexture);
-
-        window.display();
-        texture.update(image);
+        handleEvents();
+        (currentMode == mode::VIDEO) ? renderVideo() : renderMenu();
     }
 }
