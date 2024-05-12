b0c729c8d7df2bf2b1d362ea1bdaffee6c6cff4d
Valentin Peron <valentin.peron@epitech.eu>
2024-05-12 18:49:42 +0200

    Add Graphical class implementation and update render settings in Cylinder.cfg

--- src/Graphical.hpp
+++ src/Graphical.hpp
@@ -17,23 +17,38 @@
 #include "Parser.hpp"
 #include "Exceptions.hpp"
 
+enum mode {
+    VIDEO = 0,
+    MENU = 1
+};
+
 class Graphical {
     public:
         Graphical(char *path);
         ~Graphical();
         void run();
-        void eventLoop();
+        void renderVideo();
+        void renderMenu();
+        void handleEvents();
+        void handleMenuEvents();
+        void handleVideoEvents();
     private:
         sf::RenderWindow window;
-        sf::Event event;
-        sf::Texture texture;
-        sf::Sprite sprite;
         sf::Clock clock;
+        sf::Clock RenderClock;
         sf::Time time;
         sf::Font font;
         sf::Text text;
-        char *path;
+        sf::Event event;
+        sf::Image image;
+        sf::Sprite RenderedImage;
+        sf::Texture RenderedTexture;
         std::unique_ptr<RayTracer::Scene> scene;
+        RayTracer::LibLoader libLoader;
+        std::unique_ptr<RayTracer::Render::IRender> render;
+        mode currentMode;
+        char *path;
+        RayTracer::Camera *camera;
 };
 
 #endif /* !GRAPHICAL_HPP_ */
