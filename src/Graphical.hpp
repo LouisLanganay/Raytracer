8c15a67d10d7a1e6fa51d798d514a8ba05b9556d
Valentin Peron <valentin.peron@epitech.eu>
2024-05-12 22:10:06 +0200

    Refactor Graphical class to use namespace and add shaders for vertex and fragment

--- src/Graphical.hpp
+++ src/Graphical.hpp
@@ -22,33 +22,66 @@ enum mode {
     MENU = 1
 };
 
-class Graphical {
-    public:
-        Graphical(char *path);
-        ~Graphical();
-        void run();
-        void renderVideo();
-        void renderMenu();
-        void handleEvents();
-        void handleMenuEvents();
-        void handleVideoEvents();
-    private:
-        sf::RenderWindow window;
-        sf::Clock clock;
-        sf::Clock RenderClock;
-        sf::Time time;
-        sf::Font font;
-        sf::Text text;
-        sf::Event event;
-        sf::Image image;
-        sf::Sprite RenderedImage;
-        sf::Texture RenderedTexture;
-        std::unique_ptr<RayTracer::Scene> scene;
-        RayTracer::LibLoader libLoader;
-        std::unique_ptr<RayTracer::Render::IRender> render;
-        mode currentMode;
-        char *path;
-        RayTracer::Camera *camera;
-};
+namespace RayTracer::Graphical {
+
+
+    const std::string vertexShader = R"(
+        void main()
+        {
+            gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
+            gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
+        }
+    )";
+
+    const std::string gaussianBlurFragmentShader = R"(
+        uniform sampler2D texture;
+        uniform vec2 offset;
+
+        #define KERNEL_SIZE 9
+
+        void main()
+        {
+            vec2 texCoord = gl_TexCoord[0].xy;
+            vec4 color = texture2D(texture, texCoord) * 0.0625; // Center weight
+            
+            for (int i = -KERNEL_SIZE/2; i <= KERNEL_SIZE/2; ++i)
+            {
+                float weight = 1.0 / float(KERNEL_SIZE);
+                vec2 blurOffset = offset * float(i);
+                color += texture2D(texture, texCoord + blurOffset) * weight;
+            }
+
+            gl_FragColor = color;
+        }
+    )";
+    class Graphical {
+        public:
+            Graphical(char *path);
+            ~Graphical();
+            void run();
+            void renderVideo();
+            void renderMenu();
+            void handleEvents();
+            void handleMenuEvents();
+            void handleVideoEvents();
+        private:
+            sf::RenderWindow window;
+            sf::Clock clock;
+            sf::Clock RenderClock;
+            sf::Time time;
+            sf::Font font;
+            sf::Text text;
+            sf::Event event;
+            sf::Image image;
+            sf::Sprite RenderedImage;
+            sf::Texture RenderedTexture;
+            std::unique_ptr<RayTracer::Scene> scene;
+            RayTracer::LibLoader libLoader;
+            std::unique_ptr<RayTracer::Render::IRender> render;
+            mode currentMode;
+            char *path;
+            RayTracer::Camera *camera;
+    };
+}
 
 #endif /* !GRAPHICAL_HPP_ */
